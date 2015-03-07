df25ea15be18b2e43b278bad59c3f0724c8f5452
bokuwatoritori <eizawa@outlook.fr>
2015-03-07 12:31:39 -0500

    test

--- ConsoleApplication2/ConsoleApplication2/Liaison/Hamm2.cs
+++ ConsoleApplication2/ConsoleApplication2/Liaison/Hamm2.cs
@@ -8,34 +8,27 @@ namespace ConsoleApplication2.Liaison
 {
     public class Hamming
     {
-
         public struct retour
         {
             public bool erreur;
             public bool[] tabTrame;
         };
 
-        /// <summary>
-        ///  Verifie si la trame recue est correcte et retourne un tableau de bool indiquant si les bits de parite sont a la bonne valeur ou non
-        /// </summary>
-        /// <param name="tabverif"></param>
-        /// <returns>tableau de 6 bool (6 bit de parité): Si all true: Valide! on peux passer a enleveHamming. Sinon: il y a une erreur.</returns>
-        public static bool[] verifHamming(bool[] tabverif)
+        // Verifie si la trame recue est correcte et retourne un tableau de bool indiquant si les bits de parite sont a la bonne valeur ou non
+        private static bool[] verifHamming(bool[] tabverif)
         {
             bool[] taberror = new bool[6];
             // verification pour le premier bit de parité (pour le bit 1)
             int cpt = 0;
+            System.Console.WriteLine(" Début de la vérification des bits de parité de la trame reçue");
             for (int i = 2; i < 33; i += 2)
             {
                 if (tabverif[i]) cpt++;
             }
-
             if (!((((cpt % 2) == 1) && tabverif[0]) || (((cpt % 2) == 0) && !tabverif[0]))) // Premier bit de partié correct
             {
                 taberror[0] = true;
             }
-
-
             // 2e bit de parité ==> tout ce qui est 1 ou 2 modulo 4
             cpt = 0;
             for (int i = 2; i < 33; i++)
@@ -45,7 +38,6 @@ namespace ConsoleApplication2.Liaison
                     if (tabverif[i]) cpt++;
                 }
             }
-
             if (!((((cpt % 2) == 1) && tabverif[1]) || (((cpt % 2) == 0) && !tabverif[1]))) // Second bit de parité correct
             {
                 taberror[1] = true;
@@ -59,12 +51,10 @@ namespace ConsoleApplication2.Liaison
                     if (tabverif[i]) cpt++;
                 }
             }
-
             if (!((((cpt % 2) == 1) && tabverif[3]) || (((cpt % 2) == 0) && !tabverif[3]))) // Troisieme bit de parité correct
             {
                 taberror[2] = true;
             }
-
             // 4e bit de parité (8) ==> tout ce qui vaut 7 -> 14 modulo 16
             cpt = 0;
             for (int i = 8; i < 33; i++)
@@ -74,19 +64,16 @@ namespace ConsoleApplication2.Liaison
                     if (tabverif[i]) cpt++;
                 }
             }
-
             if (!((((cpt % 2) == 1) && tabverif[7]) || (((cpt % 2) == 0) && !tabverif[7]))) // Quatrième bit de parité correct
             {
                 taberror[3] = true;
             }
-
             // 5e bit de parité (16) ==> tout ce qui vaut 15 -> 30 modulo 32
             cpt = 0;
             for (int i = 16; i < 31; i++)
             {
                 if (tabverif[i]) cpt++;
             }
-
             if (!((((cpt % 2) == 1) && tabverif[15]) || (((cpt % 2) == 0) && !tabverif[15]))) // Quatrième bit de parité correct
             {
                 taberror[4] = true;
@@ -98,40 +85,35 @@ namespace ConsoleApplication2.Liaison
             }
             return taberror;
         }
-
-        public static bool[] corrigeHamming(bool[] taberror, bool[] tabTemp)
+        private static bool[] corrigeHamming(bool[] taberror, bool[] tabTemp)
         {
             int bitErrone = 0;
             bool[] tabDef = new bool[33];
+            System.Console.WriteLine(" Correction de l'erreur detectée avec Hamming");
             for (int i = 0; i < 6; i++)
             {
                 if (taberror[i])
                 {
                     bitErrone += (int)Math.Pow(2.0, (double)i);
                 }
-                if(bitErrone!=0)
+                if(bitErrone>0)
                     tabDef[bitErrone - 1] = !tabDef[bitErrone - 1];
             }
             return tabDef;
         }
-
-
-        /// <summary>
-        /// Ajoute les bits de parite a la trame que l'on souhaite envoyer. 
-        /// </summary>
-        /// <param name="trameDonnees">une trame de 27 bit SANS hamming</param>
-        /// <returns>une tramme de 33 bit avec haming</returns>
+        // Ajoute les bits de parite a la trame que l'on souhaite envoyer. La taille de la trame passera de 27 bits à 33.
+        // Retourne la trame avec Hamming ==> celle a 33 bits
         public static bool[] ajouteHamming(bool[] trameDonnees)
         {
             bool[] trameHamming = new bool[33];
             int i = 2;
             int j = 0;
+            System.Console.WriteLine(" Rajout du code de Hamming à la trame");
             for (i = 2; j < 27; i++, j++)
             {
                 if ((i == 3 || i == 7 || i == 15 || i == 31) && i < 33) i++;
                 trameHamming[i] = trameDonnees[j];
             }
-
             // verification pour le premier bit de parité (pour le bit 1) ==> un bits au numero impair (bits n° 3,5,7,9 ...)
             int cpt = 0;
             for (i = 2; i < 33; i += 2)
@@ -139,7 +121,6 @@ namespace ConsoleApplication2.Liaison
                 if (trameHamming[i]) cpt++;
             }
             if (cpt % 2 == 1) trameHamming[0] = true;
-
             // 2e bit de parité ==> tout ce qui est 1 ou 2 modulo 4
             cpt = 0;
             for (i = 2; i < 33; i++)
@@ -150,7 +131,6 @@ namespace ConsoleApplication2.Liaison
                 }
             }
             if (cpt % 2 == 1) trameHamming[1] = true;
-
             // 3e bit de parité (4) ==> tout ce qui vaut 3,4,5,6 modulo 8
             cpt = 0;
             for (i = 4; i < 33; i++)
@@ -161,8 +141,6 @@ namespace ConsoleApplication2.Liaison
                 }
             }
             if (cpt % 2 == 1) trameHamming[3] = true;
-
-
             // 4e bit de parité (8) ==> tout ce qui vaut 7 -> 14 modulo 16
             cpt = 0;
             for (i = 8; i < 33; i++)
@@ -173,8 +151,6 @@ namespace ConsoleApplication2.Liaison
                 }
             }
             if (cpt % 2 == 1) trameHamming[7] = true;
-
-
             // 5e bit de parité (16) ==> tout ce qui vaut 15 -> 30 modulo 32
             cpt = 0;
             for (i = 16; i < 31; i++)
@@ -182,85 +158,85 @@ namespace ConsoleApplication2.Liaison
                 if (trameHamming[i]) cpt++;
             }
             if (cpt % 2 == 1) trameHamming[15] = true;
-
             // 6e bit de parité (32) ==> Ne s'occupe que du dernier bit de la trame
             trameHamming[31] = trameHamming[32];
-
             return trameHamming;
         }
-
-
-        /// <summary>
-        /// Lorsque la trame recue est correcte, on peut retirer les bits de parite ajoutes par Hamming, pour remettre la trame dans sa forme originale (27 bits)
-        /// </summary>
-        /// <param name="tabHamming">une trame avec les bit de haming quon souhaite enlever</param>
-        /// <returns>sans bit de hamming (27 bit)</returns>
-        public static void enleveHamming(bool[] tabHamming, bool[] tabReception)
+        // Lorsque la trame recue est correcte, on peut donc retirer les bits de parite ajoutes par Hamming, pour remettre la trame dans sa forme originale (27 bits)
+        static bool[] enleveHamming(bool[] tabHamming)
         {
             int i = 2;
             int j = 0;
             bool[] tabDefinitif = new bool[27];
+            System.Console.WriteLine(" Suppression du code Hamming de la trame");
             for (j = 0; j < 27; i++, j++)
             {
                 if ((i == 3) || (i == 7) || (i == 15) || (i == 31) && i < 33) i++;
                 tabDefinitif[j] = tabHamming[i];
             }
-            for (int h = 0; h < Math.Min(tabReception.Length, tabDefinitif.Length); h++)
-            {
-                tabReception[h] = tabDefinitif[h];
-            }
+            return tabDefinitif;
         }
 
-
-        /// <summary>
-        /// Retorune si il y a erreur ou non et corrige si corrigble (selon bit 19)
-        /// </summary>
-        /// <param name="tabTemp">33 bit</param>
-        /// <returns></returns>
-        public static retour HammingReception(bool[] tabTemp)
+        public static retour Hamming(bool[] tabTemp)
         {
             retour R;
-            bool[] taberror = verifHamming(tabTemp);
-            bool[] tabFinale = new bool[27];
+            int cpt = 0;
             R.erreur = taberror.Any(x => x == true);
-            int QteErreurs = taberror.Count(x => x == true);
-            bool Corrigeable = false;
+            bool[] taberror = verifHamming(tabTemp);
+            //int adresse = 0;
+            //int idThread = 10; // A MODIFIER. Adresse de l'instance de thread
+            // On vérifie s'il y a effectivement des erreurs
+            System.Console.WriteLine(" La trame recue est: ");
+            AfficheTab(tabTemp, 33);
+            System.Console.WriteLine(" Les bits de parité faux sont les suivants");
+            AfficheTab(taberror, 6);
 
-            if (QteErreurs > 0 && tabTemp[19])  // on verifie dans la trame si on doit corriger en cas d'erreur
+            if (R.erreur && tabTemp[19]) // on verifie dans la trame si on doit corriger en cas d'erreur ou demander une nouvel envoi
             {
-                if (QteErreurs != 1)
+                if (cpt == 1) // S'il n'y a qu'un seul bit de parité qui ne correspond pas, ce qui n'est pas possible, il y a une erreur non corrigeable.
                 {
-                    if (taberror[5])
+                    R.erreur = true;
+                }
+                else
+                {
+                    if (taberror[5]) // Cas spécifique du dernier bit de parité (32), qui ne sert que pour un seul bit de donnée, et qui fonctionne avec le premier bit de parité
                     {
-                        if (QteErreurs == 2 && taberror[0])
+                        if (cpt == 2 && taberror[0]) // Si il n'y a que 2 bits de parité à un et que le second est le n°1, on peut corriger
                         {
-                            Corrigeable = true;
+                            tabTemp = corrigeHamming(taberror, tabTemp);
+                            enleveHamming(tabTemp);
+                            R.erreur = false;
+                        }
+                        else // Sinon, cela n'est pas possible, on se refere a une bit qui n'est pas dans la trame (n° bit > 33)
+                        {
+                            R.erreur = true;
                         }
                     }
-                    else
+                    else // pour tous les autres cas
                     {
-                        Corrigeable = true;
+                        tabTemp = corrigeHamming(taberror, tabTemp);
+                        enleveHamming(tabTemp);
+                        R.erreur = false;
                     }
                 }
             }
-
-            if (Corrigeable)
+            if (!R.erreur) // Si pas d'erreurs, on enleve hamming
             {
-                tabTemp = corrigeHamming(taberror, tabTemp);
-                enleveHamming(tabTemp,tabFinale);
-                R.erreur = false;
+                enleveHamming(tabTemp);
             }
-
-            if (!R.erreur)
-            {
-                enleveHamming(tabTemp,tabFinale);
-            }
-            R.tabTrame = new bool[tabFinale.Length];
-            for (int i = 0; i < tabFinale.Length; i++)
-            {
-                R.tabTrame[i] = tabFinale[i];
-            }
+            R.tabTrame = tabTemp;
+            System.Console.WriteLine(" La trame corrigée et nettoyée d'Hamming est: ");
+            AfficheTab(tabTemp, 27);
             return R;
+        }
+        public static void AfficheTab(bool[] tab, int tailleTab)
+        {
+            String s = " ";
+            for (int i = 0; i < tailleTab; i++)
+            {
+                s += tab[i] + " | ";
+            }
+            System.Console.WriteLine(s);
         }
     }
 }
